b92059b82f1879cc43880138a033b888c50b12a6
IceTiki <61530581+IceTiki@users.noreply.github.com>
2021-03-27 19:12:42 +0800

    格式化文档，新增自动签到失败自动重试

--- index.py
+++ index.py
@@ -12,45 +12,70 @@ def getYmlConfig(yaml_file='config.yml'):
     config = yaml.load(file_data, Loader=yaml.FullLoader)
     return dict(config)
 
+
 class Qmsg:
-    def __init__(self,config):
+    def __init__(self, config):
         # config={'key':'*****','qq':'*****','isgroup':0}
-        self.config=config
-    def send(self,msg):
+        self.config = config
+
+    def send(self, msg):
         # msg：要发送的信息|消息推送函数
-        msg=str(msg)
-        sendtype='group/' if self.config['isgroup'] else 'send/'
-        res = requests.post(url='https://qmsg.zendee.cn/'+sendtype+self.config['key'],data={'msg': msg,'qq':self.config['qq']})
+        msg = str(msg)
+        sendtype = 'group/' if self.config['isgroup'] else 'send/'
+        res = requests.post(url='https://qmsg.zendee.cn/'+sendtype +
+                            self.config['key'], data={'msg': msg, 'qq': self.config['qq']})
     #    code = res.json()['code']
     #    print(code)
 
+
 def main():
     config = getYmlConfig()
-    for user in config['users']:
-        try:
-            print('\n=========================\n'+user['user']['username']+'\n=========================\n')
-            today = TodayLoginService(user['user'])
-            today.login()
-            # 登陆成功，通过type判断当前属于 信息收集、签到、查寝
-            # 信息收集
-            if user['user']['type'] == 0:
-                # 以下代码是信息收集的代码
-                collection = Collection(today, user['user'])
-                collection.queryForm()
-                collection.fillForm()
-                msg = collection.submitForm()
-                print(msg)
-            elif user['user']['type'] == 1:
-                # 以下代码是签到的代码
-                sign = AutoSign(today, user['user'])
-                sign.getUnSignTask()
-                sign.getDetailTask()
-                sign.fillForm()
-                msg = sign.submitForm()
-                Qmsg({'key':user['user']['key'],'qq':user['user']['qq'],'isgroup':0}).send(user['user']['username']+msg)
-                print('\n=========================\n'+msg+'\n=========================\n')
-        except Exception as e:
-            print(str(e))
+    exeinfo = {}
+    exeinfo.clear
+    for i in range(7):
+        for user in config['users']:
+            try:
+                print('\n=========================\n' +
+                      user['user']['username']+'\n=========================\n')
+
+                username = user['user']['username']
+                if username in exeinfo:
+                    if not exeinfo[username] == 1:
+                        continue
+                exeinfo[username] = 1
+
+                today = TodayLoginService(user['user'])
+                today.login()
+                # 登陆成功，通过type判断当前属于 信息收集、签到、查寝
+                # 信息收集
+                if user['user']['type'] == 0:
+                    # 以下代码是信息收集的代码
+                    collection = Collection(today, user['user'])
+                    collection.queryForm()
+                    collection.fillForm()
+                    msg = collection.submitForm()
+                    print(msg)
+                elif user['user']['type'] == 1:
+                    # 以下代码是签到的代码
+                    sign = AutoSign(today, user['user'])
+                    unsigntaskExeInfo = sign.getUnSignTask()
+                    if type(unsigntaskExeInfo) != dict:
+                        exeinfo[username] = unsigntaskExeInfo
+                        continue
+                    else:
+                        unsigntaskExeInfo = unsigntaskExeInfo['taskName']
+                    sign.getDetailTask()
+                    sign.fillForm()
+                    msg = sign.submitForm()
+                    exeinfo[username] = unsigntaskExeInfo+msg
+                    Qmsg({'key': user['user']['key'], 'qq': user['user']['qq'], 'isgroup': 0}).send(
+                        username+unsigntaskExeInfo+msg)
+                    print('\n=========================\n'+unsigntaskExeInfo +
+                          '\n=========================\n'+msg+'\n=========================\n')
+            except Exception as e:
+                print(str(e))
+    print('\n==\n==\n'*10)
+    print(yaml.dump(exeinfo, allow_unicode=True))
 
 
 # 阿里云的入口函数
